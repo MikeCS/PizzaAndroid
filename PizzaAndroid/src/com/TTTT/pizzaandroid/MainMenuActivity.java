69e4ee7275a58b2c7cd8b3c9feb4def59d55cb59
Михаил <@mihail>
2013-05-24 20:18:23 +0400

    Кнопки туда-обратно

--- PizzaAndroid/src/com/TTTT/pizzaandroid/MainMenuActivity.java
+++ PizzaAndroid/src/com/TTTT/pizzaandroid/MainMenuActivity.java
@@ -1,15 +1,35 @@
 package com.TTTT.pizzaandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainMenuActivity extends Activity {
+public class MainMenuActivity extends Activity implements OnClickListener{
 
+	Button btnExitMain;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_menu);
+		btnExitMain = (Button) findViewById(R.id.btnExitMain);
+	    btnExitMain.setOnClickListener(this);
 	}
 	
+	@Override
+	  public void onClick(View v) {
+	    switch (v.getId()) {
+	    case R.id.btnExitMain:
+	    	Intent intent = new Intent(this, MainActivity.class);
+	        startActivity(intent);
+	      // TODO Call second activity
+	      break;
+	    default:
+	      break;
+	    }
 	
+	}
 }
