ac47ab3cebeb3fcabe5d69e37533f64999666951
Михаил <@mihail>
2013-05-25 22:38:50 +0400

    Спиннер заработал!!!!!!!!!!!!!!!!!!!!!!

--- PizzaAndroid/src/com/TTTT/pizzaandroid/KitchenActivity.java
+++ PizzaAndroid/src/com/TTTT/pizzaandroid/KitchenActivity.java
@@ -1,16 +1,22 @@
 package com.TTTT.pizzaandroid;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class KitchenActivity extends Activity implements OnClickListener {
 
 Button btnExitK;
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,40 @@ Button btnExitK;
 		setContentView(R.layout.kitchen_layout);
 		btnExitK = (Button) findViewById(R.id.btnExitK);
 		btnExitK.setOnClickListener(this);
+		//>>>>>>>>>>>>><<<<<<<<<<<<<
+		ArrayList<Order> orders = new ArrayList<Order>();
+		
+		Order order1 = new Order();
+		order1.Time = "17:45";
+		order1.Type = "Avalon";
+		order1.Count = 1;
+		order1.Address = "Набережная 24, кв 56";
+		order1.Status = true;
+		
+		Order order2 = new Order();
+		order2.Time = "18:01";
+		order2.Type = "Fire";
+		order2.Count = 2;
+		order2.Address = "Переверткина 20, кв 37";
+		order2.Status = true;
+		
+		Order order3 = new Order();
+		order3.Time = "18:06";
+		order3.Type = "Orlean";
+		order3.Count = 1;
+		order3.Address = "Хользунова 50, кв 57";
+		order3.Status = false;
+		
+		String[] data = {order1.Time + " " + order1.Address, order2.Time + " " + order2.Address, order3.Time + " " + order3.Address};
+		//>>>>>>>>>>>>><<<<<<<<<<<<<
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		Spinner spinner = (Spinner) findViewById(R.id.spinner);
+		spinner.setAdapter(adapter);
+		//spinner.setPrompt("Title");
+       // spinner.setSelection(2);
 	}
 
 	@Override
---
+++ PizzaAndroid/src/com/TTTT/pizzaandroid/Order.java
@@ -0,0 +1,11 @@
+package com.TTTT.pizzaandroid;
+
+public class Order {
+
+	public String Time;
+	public String Type;
+	public int Count;
+	public String Address;
+	public boolean Status;
+	
+}
