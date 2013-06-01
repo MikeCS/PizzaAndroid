54f9007e3698f29602608420e683daa793efa44d
Михаил <@mihail>
2013-06-01 13:36:10 +0400

    новые кнопки в меню

--- PizzaAndroid/gen/com/TTTT/pizzaandroid/R.java
+++ PizzaAndroid/gen/com/TTTT/pizzaandroid/R.java
@@ -22,11 +22,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int butsel=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int pizza=0x7f020003;
-        public static final int pizza1=0x7f020004;
-        public static final int pizza_slice_256=0x7f020005;
+        public static final int butmenu=0x7f020001;
+        public static final int butsel=0x7f020002;
+        public static final int button12=0x7f020003;
+        public static final int button12in=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int pizza=0x7f020006;
+        public static final int pizza1=0x7f020007;
+        public static final int pizza_slice_256=0x7f020008;
     }
     public static final class id {
         public static final int AddressText=0x7f080016;
